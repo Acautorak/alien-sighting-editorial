d97754c72cc557858c9e18c3c4af95a37c54b2e4
Utorak <acislav@gmail.com>
2024-12-08 12:46:14 +0100

    Monosingleton

---
+++ Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectsByType<T>(FindObjectsSortMode.None)[0];
+
+                if (_instance == null)
+                {
+                    GameObject singletonObject = new GameObject(typeof(T).Name);
+                    _instance = singletonObject.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+}
