a8e80b8a73bede20f7e891da90c96e02c8dee1aa
Utorak <acislav@gmail.com>
2024-12-20 21:30:54 +0100

    intro case

---
+++ Assets/Scripts/CaseFileScriptableObject.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+
+[System.Serializable]
+public class StepData
+{
+    public string clueText;
+    public Sprite clueImage;
+}
+
+[CreateAssetMenu(fileName = "NewCaseFile", menuName = "Scriptable Objects/CaseFileScriptableObject")]
+public class CaseFileScriptableObject : ScriptableObject
+{
+    public StepData[] steps = new StepData[8];
+}
