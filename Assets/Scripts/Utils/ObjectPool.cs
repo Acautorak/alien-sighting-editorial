8402b3d92aef9668e8e0a1e8e875e1dbad4a381f
Utorak <acislav@gmail.com>
2025-01-03 19:23:42 +0100

    dsdsd

--- Assets/Scripts/Utils/ObjectPool.cs
+++ Assets/Scripts/Utils/ObjectPool.cs
@@ -1,16 +1,45 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] GameObject objectPrefab;
+    [SerializeField] int initialPoolSize = 2;
+
+    private List<GameObject> pool;
+
+    private void Awake()
     {
-        
+        pool = new List<GameObject>();
+
+        for(int i = 0; i < initialPoolSize; i++)
+        {
+            GameObject obj = Instantiate(objectPrefab);
+            obj.SetActive(false);
+            pool.Add(obj);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject GetPooledObject()
     {
-        
+        foreach(GameObject obj in pool)
+        {
+            if(!obj.activeInHierarchy)
+            {
+                obj.SetActive(true);
+                return obj;
+            }
+        }
+
+        GameObject newObj = Instantiate(objectPrefab);
+        newObj.SetActive(false);
+        pool.Add(newObj);
+        newObj.SetActive(true);
+        return newObj;
+    }
+
+    public void ReturnPooledObject(GameObject gameObject)
+    {
+        gameObject.SetActive(false);
     }
 }
